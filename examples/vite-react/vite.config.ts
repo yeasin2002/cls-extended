4ca7b81ed067c25de1de6d96f4617c19f1bc01a4
yeasin2002 <mdkawsarislam2002@gmail.com>
2026-02-07 17:04:14 +0600

    chore(monorepo): integrate cls-extended core into vite-react example

--- examples/vite-react/vite.config.ts
+++ examples/vite-react/vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from "vite";
+import clsExtended from "@cls-extended/core/adapters/vite";
 import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), clsExtended()],
 });
